2303f74cf6bdffd4902f6114d5b6a5c23a11bb1b
CiugiuBackUp <corneliuciugurean@gmail.com>
2025-03-14 18:04:38 +0100

    tests folder

--- src/models/db.js
+++ src/models/db.js
@@ -1,17 +1,10 @@
 import { createPool } from 'mysql2/promise';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const connection = createPool({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_DATABASE
-  // host: "localhost",
-  // user: "root",
-  // password: "123456",
-  // database: "softwareintegration"
 });
 
 export const query = (sql, params) => {
